b3ed2f13e67954abfb917cfc22125e8a85fa2b9f
Matheus <matheus@pop-os.localdomain>
2021-06-13 22:37:53 -0300

    finished animations and transform-object lessons

--- transform-objetcts/src/script.js
+++ transform-objetcts/src/script.js
@@ -10,10 +10,29 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group()
+group.position.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xffffff })
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xff00ff })
+)
+cube3.position.x = 2
+group.add(cube3)
 
 /**
  * Sizes
@@ -27,14 +46,16 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
 camera.position.z = 3
+
 scene.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
